f884674aaf133a89236f26f8cb8e6e4dc1965c48
Abhiscoder <abhishekkumar200m1@gmail.com>
2026-01-17 12:10:43 +0530

    edge

--- src/test/java/com/abhishek/Assignment.java
+++ src/test/java/com/abhishek/Assignment.java
@@ -1,19 +1,23 @@
 package com.abhishek;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+// import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class Assignment {
 
   /*
    * 1.Assignment -Test Case
-   * i) Launch browser (chrome)
+   * i) Launch browser (chrome and edge)
    * ii) Open URL https://demo.nopcommerce.com/
    * iii) Validate title should be "nopCommerce demo store. Home page title"
    * iv) close Page
    */
   public static void main(String args[]) {
-    WebDriver driver = new ChromeDriver();
+    //Chrome browser
+    // WebDriver driver = new ChromeDriver();
+    //Edge browser
+    WebDriver driver = new EdgeDriver();
 
     driver.get("https://demo.nopcommerce.com/");
 
