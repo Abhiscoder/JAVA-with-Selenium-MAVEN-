61b9a584c0517c8b31211658950e55324c1b2736
Abhiscoder <abhishekkumar200m1@gmail.com>
2026-01-17 12:12:49 +0530

    up

--- src/test/java/com/abhishek/Assignment.java
+++ src/test/java/com/abhishek/Assignment.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 // import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
-public class Assignment {
+public class Assignment { 
 
   /*
    * 1.Assignment -Test Case
