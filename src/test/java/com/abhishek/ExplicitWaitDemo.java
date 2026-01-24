bc72d23305c33ecf05ffa969886852631ef17f28
Abhiscoder <abhishekkumar200m1@gmail.com>
2026-01-24 12:36:16 +0530

    expliciteWaitDemo

--- src/test/java/com/abhishek/ExplicitWaitDemo.java
+++ src/test/java/com/abhishek/ExplicitWaitDemo.java
@@ -1,17 +1,36 @@
 package com.abhishek;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ExplicitWaitDemo {
   public static void main(String[] args) {
     WebDriver driver = new ChromeDriver();
+    WebDriverWait mywait = new WebDriverWait(driver, Duration.ofSeconds(10)); // declaration
+
     driver.manage().window().maximize();
 
     driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 
-    driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys("Admin");
-    driver.quit();
+    WebElement txtusername = mywait
+        .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@placeholder='Username']")));
+
+    txtusername.sendKeys("Admin");
+    WebElement txtpassword = mywait
+        .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@placeholder='Password']")));
+
+    txtpassword.sendKeys("admin123");
+
+    WebElement btnlogin = mywait
+        .until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@type='submit']")));
+
+    btnlogin.click();
+    // driver.quit();
   }
 }
