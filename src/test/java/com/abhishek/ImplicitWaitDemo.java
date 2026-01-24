b66be4c6fa8959c1726dad84c4ba62af7ec04f0a
Abhiscoder <abhishekkumar200m1@gmail.com>
2026-01-24 12:10:15 +0530

    implicit wait

---
+++ src/test/java/com/abhishek/ImplicitWaitDemo.java
@@ -0,0 +1,22 @@
+package com.abhishek;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ImplicitWaitDemo {
+  public static void main(String args[]) {
+
+    WebDriver driver = new ChromeDriver();
+    
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5)); //Implicit Wait
+    driver.manage().window().maximize();
+    driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+    driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys("Admin");
+
+    driver.quit();
+
+  }
+}
