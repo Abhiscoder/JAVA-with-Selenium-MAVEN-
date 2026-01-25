1e2f21516762e525462b93c8f575ebb5881bb213
Abhiscoder <abhishekkumar200m1@gmail.com>
2026-01-25 10:41:51 +0530

    Navigational commands

--- src/test/java/com/abhishek/NavigationalCommands.java
+++ src/test/java/com/abhishek/NavigationalCommands.java
@@ -7,8 +7,19 @@ public class NavigationalCommands {
   public static void main(String args[]) {
     WebDriver driver = new ChromeDriver();
 
-    driver.get("https://demo.nopcommerce.com/");  //open page and accepts URL in the string format
-    // driver.navigate().to("https://demo.nopcommerce.com/"); //open page with history and accepts URL in the string format and object format
+    // driver.get("https://demo.nopcommerce.com/"); //open page and accepts URL in
+    // the string format
+    driver.navigate().to("https://demo.nopcommerce.com/"); // open page with history and accepts URL in the string
+                                                           // format and object format
+    driver.navigate().to("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+    driver.navigate().back();
+    System.out.println(driver.getCurrentUrl());
+
+    driver.navigate().forward();
+    System.out.println(driver.getCurrentUrl());
+
+    driver.navigate().refresh();
   }
 
 }
