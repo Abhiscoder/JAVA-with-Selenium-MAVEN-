9a4bce70b91c4ba484acd30b5573d9fe4d03469d
Abhiscoder <abhishekkumar200m1@gmail.com>
2026-01-25 11:12:04 +0530

    handle browser windows

--- src/test/java/com/abhishek/HandleBrowserWindows.java
+++ src/test/java/com/abhishek/HandleBrowserWindows.java
@@ -2,7 +2,8 @@ package com.abhishek;
 
 import java.time.Duration;
 import java.util.Set;
-
+import java.util.ArrayList;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -20,14 +21,28 @@ public class HandleBrowserWindows {
 
     Set<String> windowIDs = driver.getWindowHandles();
 
-    //Apporach 1
-    List<String> windowList  = new ArrayList(windowIDs);
+    // Apporach 1
+    /*
+     * List<String> windowList = new ArrayList<String>(windowIDs);
+     * 
+     * String parentID = windowList.get(0);
+     * String childID = windowList.get(1);
+     * 
+     * // Switch to child window
+     * driver.switchTo().window(childID);
+     * System.out.println(driver.getTitle());
+     * 
+     * // Switch to parent window
+     * driver.switchTo().window(parentID);
+     * System.out.println(driver.getTitle());
+     */
 
-    String parentID = windowList.get(0);
-    String childID = windowList.get(1);
-
-    System.out.println(driver.getTitle());
-    
-
+    // Approach 2
+    for (String windId : windowIDs) {
+      String title =driver.switchTo().window(windId).getTitle();
+      if (title.equals("OrnageHRM")) {
+        System.out.println(driver.getCurrentUrl()); //some validation on the parent window
+      }
+    }
   }
 }
