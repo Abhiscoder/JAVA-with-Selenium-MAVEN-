4dd4becd4b4a4e437444901be3d19fbaf66d64a8
Abhiscoder <abhishekkumar200m1@gmail.com>
2026-01-29 11:51:25 +0530

    printing dorpdown and count

--- src/test/java/com/abhishek/SelectDropDown.java
+++ src/test/java/com/abhishek/SelectDropDown.java
@@ -1,5 +1,7 @@
 package com.abhishek;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -18,13 +20,27 @@ public class SelectDropDown {
 
     // Select option from the drop down
     // 1) selectByVisibleText();
-    dropCountry.selectByVisibleText("France");//France
+    dropCountry.selectByVisibleText("France");// France
 
     // 2) selectByValue();
-    dropCountry.selectByValue("uk");//United Kingdom
+    // dropCountry.selectByValue("uk");//United Kingdom
 
     // 3)selectByIndex();
-    dropCountry.selectByIndex(3);// Germany
+    // dropCountry.selectByIndex(3);// Germany
 
+    // Capture the options from the dropdown
+    List<WebElement> options = dropCountry.getOptions();
+    System.out.println("Totel number of options in dropdown:" + options.size()); // 10
+
+    // Printing the options
+    // for (int i = 0; i < options.size(); i++) {
+    // System.out.println(options.get(i).getText());
+    // }
+
+    // Printing the options using Enhance loop
+    for (WebElement option : options) {
+      System.out.println(option.getText());
+
+    }
   }
 }
